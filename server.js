cdd02e7c85461aa8aeedd7ba3d3ed519bb10aeae
MallikAdi <aditya.mallik.10@gmail.com>
2024-05-31 19:18:18 +0530

    hi

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const request = require("request");
 const app = express();
 
+// Respond with "Hi" when accessing the root URL
+app.get("/", (req, res) => {
+  res.send("Hi there!");
+});
+
+// Proxy all other requests
 app.use((req, res) => {
   const url = `https://www.irflabs.in${req.url}`;
   req.pipe(request(url)).pipe(res);
