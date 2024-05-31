cf4bec651f3db6226bf1b3484556ca1402883b85
MallikAdi <aditya.mallik.10@gmail.com>
2024-05-31 21:09:22 +0530

    retry1

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use((req, res) => {
   req.pipe(request(url)).pipe(res);
 });
 
-app.listen(3001, () => {
-  console.log("Proxy server is running on port 3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on port ${PORT}`);
 });
